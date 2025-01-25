a3f04103f565ad0180d2498c567a906405c8fc27
Ryan Dunagan <Ryan.Dunagan.Dev@gmail.com>
2025-01-25 16:24:20 -0700

    Bubble spline fixes

--- GGJ25-BubbleKatamari/Assets/Scripts/Environment/BK_BubbleSpline.cs
+++ GGJ25-BubbleKatamari/Assets/Scripts/Environment/BK_BubbleSpline.cs
@@ -9,14 +9,8 @@ public class BK_BubbleSpline : MonoBehaviour
     [SerializeField] private GameObject bubblePrefab;
 
     [SerializeField] private int numBubbles = 10;
-    [SerializeField] private AnimationCurve bubbleDistribution;
-
-#if UNITY_EDITOR
-    private void OnValidate()
-    {
-        PlaceBubbles();
-    }
-#endif
+    [SerializeField] private AnimationCurve bubblePositionDistribution;
+    [SerializeField] private AnimationCurve bubbleSizeDistribution;
 
     public void PlaceBubbles()
     {
@@ -28,14 +22,14 @@ public class BK_BubbleSpline : MonoBehaviour
 
         for (int i = 0; i < numBubbles; i++)
         {
-            float pos = (float)i / (numBubbles - 1);
-            float3 splinePos = splineContainer.Spline.EvaluatePosition(pos);
-            //splinePos += splineStart;
+            float pos = (float)i / (numBubbles - (splineContainer.Spline.Closed ? 0 : 1));
+            float3 splinePos = splineContainer.Spline.EvaluatePosition(bubblePositionDistribution.Evaluate(pos));
 
             Debug.Log($"Spawn: {i}, {pos}, {splinePos}");
 
             GameObject newBubble = Instantiate(bubblePrefab, transform);
             newBubble.transform.localPosition = splinePos;
+            newBubble.transform.localScale = Vector3.one * bubbleSizeDistribution.Evaluate(pos);
         }
     }
 
