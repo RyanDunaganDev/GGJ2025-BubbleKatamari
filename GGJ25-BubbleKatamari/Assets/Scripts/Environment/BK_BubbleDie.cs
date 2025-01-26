8716f77455b38e69a159a3fce69c730b4a19b7d7
Neal Finger <spiftastic@msn.com>
2025-01-25 22:16:47 -0700

    Player die animation
    script used to control the shader parameter

--- GGJ25-BubbleKatamari/Assets/Scripts/Environment/BK_BubbleDie.cs
+++ GGJ25-BubbleKatamari/Assets/Scripts/Environment/BK_BubbleDie.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+using Unity.Mathematics;
 
+[RequireComponent(typeof(Material))]
 public class BK_BubbleDie : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float durtion = 0.5f;
+    private string paramName = "IsPop";
+    private Material mat = null;
+
+
+    public void RunDieAnimation()
     {
-        
+        StartCoroutine(CoDieAnimtation());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CoDieAnimtation()
     {
-        
+        float duratedTime = 0f;
+        float isPop = 0f;
+        while (duratedTime < durtion)
+        {
+            duratedTime += Time.deltaTime;
+            mat.SetFloat(paramName, math.remap(0f, duratedTime, -1f, 1, duratedTime));
+            yield return false;
+        }
+        yield return true;
+    }
+
+    void Activate()
+    {
+        mat = GetComponent<Material>();
     }
 }
